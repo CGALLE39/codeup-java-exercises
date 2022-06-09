3946826aa7bf66feba12f27ebca69b03f11cbdcd
Cynthia Gallegos <cynthialgallegos97@gmail.com>
2022-06-09 14:25:52 -0500

    operators

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,41 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        byte tinyNum = 127; //Stores whole numbers from -128 to 127
+        System.out.println(tinyNum);
+        short myNum = 5000; //stores whole numbers from -32768-32767
+        System.out.println(myNum);
+        int myFavoriteNumber = 39; //integer
+        System.out.println(myFavoriteNumber);
+        boolean myBoo = true; //boolean
+        System.out.println(myBoo);
+        char myLetter = 'C'; //character
+        System.out.println(myLetter);
+        float myFloatNum = 3.99f; //floating number
+        System.out.println(myFloatNum);
+        double dblNum = 19.99d;
+        System.out.println(dblNum);
+        boolean isJavaFun = true;
+        System.out.println(isJavaFun);     // Outputs true
+        boolean isFishTasty = false;
+        System.out.println(isFishTasty);   // Outputs false
+        String myText = "Awesome"; //String
+        System.out.println(myText);
         System.out.println("Hello, World!");
         System.out.println("Hello, again");
-        System.out.print("Hello, test");
+        System.out.println("Hello, test");
+        char myVar1 = 65, myVar2 = 66, myVar3 = 67;
+        System.out.println(myVar1);
+        System.out.println(myVar2);
+        System.out.println(myVar3);
+        //////////
+        int newInt = 10;
+        double addDouble = newInt;
+        System.out.println(newInt);
+        System.out.println(addDouble);
+        int x = 100 + 50;
+        System.out.println(x);
+        int sum1 = 30 * 4;
+        System.out.println(sum1);
     }
 }
+
