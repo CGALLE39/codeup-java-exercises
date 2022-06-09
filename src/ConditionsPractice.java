f35ac1577f11ab413a90ba9c9fe4665aa05e840c
Cynthia Gallegos <cynthialgallegos97@gmail.com>
2022-06-09 15:51:03 -0500

    conditions

---
+++ src/ConditionsPractice.java
@@ -0,0 +1,19 @@
+public class ConditionsPractice {
+    public static void main(String[] args) {
+        if (20 > 18) {
+            System.out.println("20 is greater than 18");
+        }
+
+        int luckyNum = 7;
+        if (luckyNum == 7) {
+            System.out.println("You win!");
+        } else {
+            System.out.println("You win NOTHING");
+        }
+
+        int luckyNumber = 8;
+        String result = (luckyNumber == 7) ? "You win!" : "You win NOTHING";
+        System.out.println(result);
+
+    }
+}
